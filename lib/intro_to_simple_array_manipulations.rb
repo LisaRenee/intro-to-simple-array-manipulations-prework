a5e74a4e1a70316a2975b24cbbf8dffe17340d39
Lisa Scott <lrscot@aol.com>
2019-02-06 07:25:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -35,7 +35,7 @@ def using_uniq(captain_planet_and_the_planeteers)
 captain_planet_and_the_planeteers.uniq
 end
 
-#def using_flatten(private_colleges_in_newyork)
+def using_flatten(private_colleges_in_newyork)
   private_colleges_in_newyork.flatten
 end
 
